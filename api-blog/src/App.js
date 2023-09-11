1d02fcff27027fdf1b8f4ce5ebf3d230942e92b0
mohammedmoin2001 <124486931+mohammedmoin2001@users.noreply.github.com>
2023-09-11 09:48:32 +0530

    Weather App

---
+++ api-blog/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import Weather from './Weather';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
